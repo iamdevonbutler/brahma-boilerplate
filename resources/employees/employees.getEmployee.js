9cec5093f46de1df37d51af2abbc164425670427
iamdevonbutler <jpescione@gmail.com>
2017-12-08 10:12:21 -0500

    ...

--- resources/employees/employees.getEmployee.js
+++ resources/employees/employees.getEmployee.js
@@ -1,8 +1,12 @@
 module.exports = {
   route: '',
-  docs: [],
+  method: 'GET', // or 'get', or undefined === 'get'
+  docs: [
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. At consequatur iusto quod unde, cum sint voluptatem aspernatur dignissimos iure dolorem voluptas quas eaque quis dolor quae ratione atque. Odio, quisquam.',
+    '...',
+  ],
   test() {
-    
+
   },
   main() {
 
